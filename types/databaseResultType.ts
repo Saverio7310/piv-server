4dfabec4c9d1a2059f6d45cbcab0f15d29202fc4
Saverio Perrone <saverio.perrone20@gmail.com>
2025-01-28 15:08:04 +0100

    add database types

---
+++ types/databaseResultType.ts
@@ -0,0 +1,39 @@
+export interface Product {
+    name: string,
+    name_token: string,
+    product_id: number,
+    code: number,
+    family_id: number | null,
+    unit: string,
+    value: number,
+    trolley_department_id: number,
+    intervals: any[],
+    values: any[],
+    created_at: string | null,
+    //updated_at: string | null
+    //[key: string]: any;
+}
+
+export interface ProductInfo {
+    product: Product,
+    prices: ProductPrice
+}
+
+export interface ProductPrice {
+    product_id: number,
+    price: number,
+    original_price: number,
+    original_discounted_price: number,
+    unit_price: number,
+    discounted_price: number,
+    discounted_unit_price: number
+    label: string,
+    created_at: string | null,
+    //updated_at: string | null
+    //[key: string]: any;
+}
+
+export type DBResult = {
+    rowCount: number | null,
+    data: any[],
+}
