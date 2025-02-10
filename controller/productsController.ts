dee7c7bfb10362fd6c589e92acc7d55e757fd21a
Saverio Perrone <saverio.perrone20@gmail.com>
2025-02-10 17:12:06 +0100

    update controllers with cache

--- controller/productsController.ts
+++ controller/productsController.ts
@@ -1,68 +1,30 @@
 import axios from 'axios';
 import { Request, Response, NextFunction } from 'express';
 import { getProductPrice, getProductsInfo } from '../database/database';
-
-export const getProducts_ = (req: Request, res: Response, next: NextFunction) => {
-    const data = req.query;
-    console.log('incoming data', data);
-    if (!data || !data.limit || !data.offset) {
-        console.log('No params found!');
-        const data = {
-            message: 'Missing pagination values',
-        };
-        res.setHeader('Content-Type', 'application/json');
-        res.status(400).send(JSON.stringify(data));
-        return;
-    }
-    axios({
-        method: 'get',
-        url: `https://jsonplaceholder.typicode.com/posts`,
-        params: {
-            userId: data.iteration,
-        },
-        timeout: 2000,
-    })
-        .then(function (response) {
-            const data = response.data;
-            res.setHeader('Content-Type', 'application/json');
-            res.status(response.status).send(JSON.stringify(data));
-        })
-        .catch(function (error) {
-            if (error.code === 'ECONNABORTED' || error.code === 'ETIMEDOUT') {
-                console.error('Request timeout reached');
-                res.sendStatus(408);
-                return;
-            }
-            if (error.response) {
-                console.error('Server replied with an error');
-                const data = {
-                    message: error.response.message,
-                };
-                res.setHeader('Content-Type', 'application/json');
-                res.status(error.response.status).send(JSON.stringify(data));
-            } else if (error.request) {
-                console.error('Server did not replied');
-                const data = error.request;
-                res.setHeader('Content-Type', 'application/json');
-                res.status(error.status).send(JSON.stringify(data));
-            } else {
-                console.error('Error occurred in request setting up');
-                res.sendStatus(400);
-            }
-        })
-};
+import { RedisWrapper } from '../model/RedisWrapper';
+import { matchedData } from 'express-validator';
 
 export const getProducts = async (req: Request, res: Response, next: NextFunction) => {
-    const searchQuery = req.params.searchQuery.split('-').join(' ').replace('%25', '%');
-    const offset = parseInt(req.query.offset as string) || 0;
+    /* const searchQuery = req.params.searchQuery.split('-').join(' ').replace('%25', '%');
+    const offset = parseInt(req.query.offset as string) || 0; */
+    const data = matchedData(req);
+    const searchQuery: string = data.searchQuery;
+    const offset: number = data.offset;
     const limit = 10 //parseInt(req.params.limit);
 
-    if (!searchQuery /* || !offset || !limit */ || isNaN(offset) || isNaN(limit)) {
-        res.sendStatus(404);
-        return;
-    }
+    /**
+     * The cache flow should be
+     * 1 - check for data inside cache
+     *  Y -> return that
+     *  N -> go ahead
+     * 2 - fetch data from DB
+     * 3 - save data inside redis
+     * 4 - return DB fetched data
+     */
 
-    const result = await getProductsInfo(searchQuery, offset, limit);
+    const cache = await RedisWrapper.getInstance();
+    const cacheKey = `products:${searchQuery}?offset=${offset}`;
+    const result = await cache.cacheData(cacheKey, () => getProductsInfo(searchQuery, offset, limit));
 
     if (result.rowCount === null) {
         next(new Error('Database connection error'));
@@ -73,15 +35,14 @@ export const getProducts = async (req: Request, res: Response, next: NextFunctio
 };
 
 export const getProductInfo = async (req: Request, res: Response, next: NextFunction) => {
-    const product_id = req.params.product_id;
-
-    if (!product_id) {
-        next(new Error('Missing product identifier'));
-        return;
-    }
-
-    const result = await getProductPrice(product_id);
-
+    //const product_id = req.params.product_id;
+    const data = matchedData(req);
+    const product_id: string = data.product_id;
+    
+    const cache = await RedisWrapper.getInstance();
+    const cacheKey = `prices:${product_id}`;
+    const result = await cache.cacheData(cacheKey, () => getProductPrice(product_id));
+    
     if (result.rowCount === null) {
         next(new Error('Database connection error'));
         return;
