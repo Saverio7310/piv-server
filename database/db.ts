b492e43e936f7bebf047c2cf7068605ad3427a9b
Saverio Perrone <saverio.perrone20@gmail.com>
2025-02-10 17:10:48 +0100

    add env vars

--- database/db.ts
+++ database/db.ts
@@ -1,12 +1,12 @@
 import { Pool } from 'pg'
 
 const pool = new Pool({
-    user: 'saverioperrone',
-    host: '192.168.1.147',
-    database: 'prezziinvista',
-    password: 'modtoS-mudtuz-2gicnu',
-    port: 5432,
-    connectionTimeoutMillis: 2000
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_DATABASE,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT) : 5432,
+    connectionTimeoutMillis: process.env.DATABASE_TIMEOUT ? parseInt(process.env.DATABASE_TIMEOUT) : 2000,
 });
 
 export default pool;
