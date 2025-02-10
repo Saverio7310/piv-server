3618f69e56838d8db112db010702a02e876cf686
Saverio Perrone <saverio.perrone20@gmail.com>
2025-02-10 17:12:46 +0100

    add input validations and rate limiter

--- routes/productsRouter.ts
+++ routes/productsRouter.ts
@@ -1,10 +1,40 @@
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import { getProducts, getProductInfo } from '../controller/productsController';
+import { body, param, query, validationResult } from 'express-validator';
+import rateLimiter from '../middleware/rateLimiter';
 
 export const router = Router();
 
-//router.get('/products', getProducts);
+const handleValidationErrors = (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({errors: errors.array()});
+        return;
+    }
+    next();
+};
 
-router.get('/products/:searchQuery', getProducts)
+router.use('*', rateLimiter);
 
-router.get('/products/:product_id/info', getProductInfo)
+router.get('/products/:searchQuery',
+    [
+        param('searchQuery')
+            .trim()
+            .isLength({min: 3, max: 50}).withMessage('Input string\'s length out of bounds')
+            .customSanitizer((value) => value.split('-').join(' ').replace('%25', '%')),
+        query('offset')
+            .trim()
+            .isInt({min: 0, max: 200}).withMessage('Offset value out of bounds')
+            .toInt()
+    ],
+    handleValidationErrors, 
+    getProducts);
+
+router.get('/products/:product_id/info', 
+    [
+        param('product_id')
+            .trim()
+            .isInt({min: 0}).withMessage('Invalid product\'s identifier')
+    ],
+    handleValidationErrors, 
+    getProductInfo);
