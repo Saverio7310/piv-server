6d73838274a38877c95efd49ad986ee779360324
Saverio Perrone <saverio.perrone20@gmail.com>
2025-02-28 16:53:34 +0100

    change postgresql connection logic

--- database/db.ts
+++ database/db.ts
@@ -1,12 +1,10 @@
 import { Pool } from 'pg'
 
 const pool = new Pool({
-    user: process.env.DATABASE_USER,
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_DATABASE,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT) : 5432,
-    connectionTimeoutMillis: process.env.DATABASE_TIMEOUT ? parseInt(process.env.DATABASE_TIMEOUT) : 2000,
+    connectionString: process.env.DATABASE_URL,
+    connectionTimeoutMillis: process.env.DATABASE_CONN_TIMEOUT ? parseInt(process.env.DATABASE_CONN_TIMEOUT) : 10000,
+    idleTimeoutMillis: process.env.DATABASE_IDLE_TIMEOUT ? parseInt(process.env.DATABASE_IDLE_TIMEOUT) : 30000,
+    max: 50
 });
 
 export default pool;
