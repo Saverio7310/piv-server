7a635be2b4602140bb2bf407e0c1ac7b6d9d3dfc
Saverio Perrone <saverio.perrone20@gmail.com>
2025-06-19 18:55:05 +0200

    update old queries and add new one to fetch and gather data from multiple supermarkets at once

--- database/database.ts
+++ database/database.ts
@@ -1,10 +1,11 @@
 import { QueryResult } from 'pg';
 import pool from './db';
-import { ProductPrice, Product, DBResult, ProductInfo } from '../types/databaseResultType';
+import { ProductPrice, Product, DBResult, ProductInfo, Prices } from '../types/databaseResultType';
+import { SupermarketData } from '../types/productType';
 
-export async function tryConnection(table: string) {
+export async function tryConnection() {
     const query = {
-        text: `SELECT * FROM ${table} LIMIT 1 OFFSET 0`,
+        text: `SELECT 1`,
         values: [],
     }
     try {
@@ -15,7 +16,7 @@ export async function tryConnection(table: string) {
     }
 }
 
-export async function getProducts(table: string, offset: number, limit: number) {
+export async function getProducts(table: string, date: string, offset: number, limit: number) {
     const queryString = `
         SELECT
         (prod ->> 'id')::integer AS product_id,
@@ -37,12 +38,13 @@ export async function getProducts(table: string, offset: number, limit: number) 
         prod ->> 'label' != '' AND
         prod ->> 'productType' != 'VIRTUAL' AND 
         prod -> 'premium' != 'true' AND 
-        prod -> 'quantityDisabled' != 'true'
+        prod -> 'quantityDisabled' != 'true' AND
+        created_at = $1
         ORDER BY product_id
-        OFFSET ${offset} LIMIT ${limit};`;
+        OFFSET $2 LIMIT $3;`;
     const query = {
         text: queryString,
-        values: [],
+        values: [date, offset, limit],
     }
     let res: QueryResult<Product>;
     try {
@@ -58,6 +60,7 @@ export async function saveProduct(table: string, prod: Product, created_at: stri
     const queryString = `
         INSERT INTO ${table}(product_id, unit, value, code, family_id, trolley_department_id, name, name_token, intervals, values, created_at, updated_at)
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, DEFAULT)
+        ON CONFLICT (product_id,created_at) DO NOTHING
     `;
     const jsonIntervals = JSON.stringify(prod.intervals);
     const jsonValues = JSON.stringify(prod.values);
@@ -86,7 +89,7 @@ export async function saveProduct(table: string, prod: Product, created_at: stri
  * @param limit 
  * @returns 
  */
-export async function getPrettifiedProducts(table: string, offset: number, limit: number) {
+export async function getPrettifiedProducts(table: string, date: string, offset: number, limit: number) {
     const queryString = `
         SELECT 
         name, 
@@ -112,12 +115,13 @@ export async function getPrettifiedProducts(table: string, offset: number, limit
         JOIN
         prices_esselunga p2
         ON p1.product_id = p2.product_id and p1.created_at = p2.created_at
+        WHERE p1.created_at = $1
         ORDER BY p1.product_id
-        OFFSET ${offset} LIMIT ${limit};
+        OFFSET $2 LIMIT $3;
     `;
     const query = {
         text: queryString,
-        values: [],
+        values: [date, offset, limit],
     }
     let res: QueryResult<any>;
     try {
@@ -181,18 +185,19 @@ export async function saveProductPrice(table: string, price: ProductPrice, creat
     const queryString = `
         INSERT INTO ${table}(product_id, label, original_price, original_discounted_price, price, unit_price, discounted_price, discounted_unit_price, created_at, updated_at)
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, DEFAULT)
+        ON CONFLICT (product_id,created_at) DO NOTHING
     `;
     const query = {
         text: queryString,
         values: [
-            price.product_id, 
-            price.label, 
-            price.original_price, 
-            price.original_discounted_price, 
-            price.price, 
-            price.unit_price, 
-            price.discounted_price, 
-            price.discounted_unit_price, 
+            price.product_id,
+            price.label,
+            price.original_price,
+            price.original_discounted_price,
+            price.price,
+            price.unit_price,
+            price.discounted_price,
+            price.discounted_unit_price,
             created_at]
     };
 
@@ -240,6 +245,69 @@ export async function getProductsInfo(searchQuery: string, offset: number, limit
     return fetchedResult;
 }
 
+export async function getSupermarkets() {
+    const query = 'SELECT ARRAY_AGG(supermarket) AS supermarkets_array FROM supermarkets';
+    const fetchedResult: DBResult = {
+        rowCount: null,
+        data: []
+    }
+    try {
+        const result = await pool.query(query);
+        fetchedResult.rowCount = result.rowCount;
+        fetchedResult.data = result.rows[0].supermarkets_array;
+    } catch (error) {
+        console.error('Error while fetching supermarkets', error);
+    }
+    return fetchedResult;
+}
+
+export async function getProductsInfo2(searchQuery: string, offset: number, limit: number) {
+    const fetchedResult: DBResult = {
+        rowCount: null,
+        data: []
+    }
+    const supermarkets = await getSupermarkets();
+    if (!supermarkets.rowCount) return fetchedResult;
+
+    for (const supermarket of supermarkets.data) {
+        const queryString = `
+            SELECT mp.master_id, mp.name AS master_name, mp.brand AS master_brand, mp.quantity_unit AS master_quantity_unit, mp.quantity_value AS master_quantity_value, pe.product_id, pe.name, pe.brand, pe.quantity_unit, pe.quantity_value, '${supermarket}' AS supermarket_name
+            FROM master_products mp
+            JOIN link_products lp
+            ON mp.master_id = lp.master_id 
+            RIGHT OUTER JOIN (
+                SELECT /* DISTINCT ON (product_id) */ *
+                FROM products_${supermarket} peo
+                WHERE name ILIKE '%' || $1 || '%' AND peo.created_at = (
+                    SELECT created_at
+                    FROM products_${supermarket} pei
+                    WHERE pei.product_id = peo.product_id
+                    ORDER BY pei.created_at DESC
+                    LIMIT 1
+                )
+                ORDER BY product_id asc, created_at DESC
+            ) AS pe
+            ON lp.product_id = pe.product_id
+            WHERE mp.name ILIKE '%' || $1 || '%' OR mp.name IS NULL
+            ORDER BY mp.name, mp.quantity_value, pe.name, pe.quantity_value
+            OFFSET $2 LIMIT $3;
+        `;
+        const query = {
+            text: queryString,
+            values: [searchQuery, offset, limit]
+        }
+
+        try {
+            const result = await pool.query(query);
+            fetchedResult.data.push(...result.rows);
+        } catch (error) {
+            console.error('Error while fetching data', error);
+        }
+    }
+    fetchedResult.rowCount = fetchedResult.data.length ? fetchedResult.data.length : null;
+    return fetchedResult;
+}
+
 export async function getProductPrice(product_id: string) {
     const queryString = `
     SELECT 
@@ -268,5 +336,58 @@ export async function getProductPrice(product_id: string) {
     } catch (error) {
         console.error('Error while fetching product\'s price info', error);
     }
+    return fetchedResult;
+}
+
+export async function getProductPrice2(supermarkets: SupermarketData[]) {
+    const fetchedResult: DBResult = {
+        rowCount: null,
+        data: []
+    }
+    for (const el of supermarkets) {
+        const queryString = `
+            SELECT 
+            product_id, 
+            CAST(created_at AS text), 
+            price, 
+            unit_price, 
+            discounted_price, 
+            discounted_unit_price
+            FROM prices_${el.supermarket}
+            WHERE product_id = $1
+            ORDER BY created_at ASC;
+        `;
+        const query = {
+            text: queryString,
+            values: [el.origin_id]
+        }
+
+        try {
+            const result = await pool.query(query);
+
+            const prices: Prices = {
+                supermarket_name: el.supermarket,
+                product_id: el.origin_id,
+                unit_prices: [],
+                prices: [],
+                discounted_prices: [],
+                discounted_unit_prices: [],
+                dates: [],
+            }
+
+            result.rows.forEach(obj => {
+                prices.prices.push(obj.price),
+                prices.unit_prices.push(obj.unit_price),
+                prices.discounted_prices.push(obj.discounted_price),
+                prices.discounted_unit_prices.push(obj.discounted_unit_price),
+                prices.dates.push(obj.created_at)
+            });
+
+            fetchedResult.data.push(prices);
+        } catch (error) {
+            console.error('Error while fetching product\'s price info', error);
+        }
+    }
+    fetchedResult.rowCount = fetchedResult.data.length ? fetchedResult.data.length : null;
     return fetchedResult;
 }
